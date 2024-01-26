23bdf1d759e29812103a3d43287f04634b0292dc
Beshoy_Refaat <beshoy.refaat1919@gmail.com>
2024-01-26 22:59:19 +0200

    andrew mead course lesson one

---
+++ week_one/main.js
@@ -0,0 +1,18 @@
+//say hello to test setup section one
+console.log("hello js");
+// 1- string and variables
+let name = 'Beshoy Refaat'; 
+console.log(name);
+let first_name = 'Beshoy ';
+let second_name = 'Refaat';
+let full_name = first_name + second_name;
+console.log(full_name);
+//challenge 
+let city = 'Luxor';
+let country = 'Egypt';
+let Location = city + ', ' +country;
+console.log(Location);
+// 2- Numbers
+
+
+
