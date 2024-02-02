cef2b83ad91b0673cbdb5dc913c55bee4493c210
Beshoy_Refaat <beshoy.refaat1919@gmail.com>
2024-02-02 21:25:19 +0200

    #

--- week_one/boolean.js
+++ week_one/boolean.js
@@ -1,4 +1,19 @@
-let temp = 31;
-let isFreezing = true;
+let temp = 0;
+//if temp is 0 or less than 0 its already frezing
+let isFreezing = temp === 31;
+//=== - equality operator
+console.log(isFreezing); // result is false
+// !== - not equal operator
+// < >  <= >= operators
 
-console.log(isFreezing);
+//challenge
+// create age variable
+let age = 66;
+//calculate is child - if they are 7 or under
+let isChild = age <= 7;
+//calculate is senior - if they are 65 or older
+let isSenior = age >= 65;
+//print is child value
+console.log("is child value: " + isChild);
+//print is senior value
+console.log("is senior value: " + isSenior);
